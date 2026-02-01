7f183515a5b4314b617fe053fb00f26202228154
Yegor Bugayenko <yegor256@gmail.com>
2026-02-01 06:47:30 +0300

    Update Gemfile and Gemfile.lock versions of gems.

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -3,11 +3,28 @@
 
 $stdout.sync = true
 
+require 'simplecov'
 require 'simplecov-cobertura'
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+unless SimpleCov.running || ENV['PICKS']
+  SimpleCov.command_name('test')
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::CoberturaFormatter
+    ]
+  )
+  SimpleCov.minimum_coverage 85
+  SimpleCov.minimum_coverage_by_file 60
+  SimpleCov.start do
+    add_filter 'test/'
+    add_filter 'vendor/'
+    add_filter 'target/'
+    track_files 'lib/**/*.rb'
+    track_files '*.rb'
+  end
+end
 
+require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 Minitest.load :minitest_reporter
-
-require 'minitest/autorun'
